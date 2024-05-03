df8d62795277c45af17e90a518b2d5cfb5b48593
Alisa Dubina <adubina24@stu.aberdeenhall.com>
2024-05-03 13:10:38 -0700

    Goose learned how to talk

--- Assets/Script/DialogueManager.cs
+++ Assets/Script/DialogueManager.cs
@@ -1,18 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class DialogueManager : MonoBehaviour
 {
+    public TMP_Text nameText;
+    public TMP_Text dialogueText;
+    private Queue<string> sentences;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    void Start() {
+        sentences = new Queue<string>();
+    } // Start
+
+    public void StartDialogue(Dialogue dialogue) {
+        Debug.Log("Starting conversation with " + dialogue.character);
+        nameText.text = dialogue.character;
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences) {
+            sentences.Enqueue(sentence);
+        }// foreach
+
+        DisplayNextSentence();
+    } // StartDialogue
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+    void Update() {
+        if (Input.GetKeyDown("space")) {
+            DisplayNextSentence();
+        } // if 
+    } // Update
+
+    public void DisplayNextSentence() {
+        if(sentences.Count == 0) {
+            EndDialogue();
+            return;
+        } // if
+
+        string sentence = sentences.Dequeue();
+        dialogueText.text = sentence;
+
+    }// DisplayNextSentence
+
+    void EndDialogue() {
+        Debug.Log("End of conversation.");
+    } // EndDialogue
+} // DialogueManager
