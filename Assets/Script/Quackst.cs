ccd6ebfd46bf00fdc62a72bd23d139b79d190dc9
Amelie Zhang <amelie.ry.zh@gmail.com>
2024-05-09 19:23:11 -0700

    quack

---
+++ Assets/Script/Quackst.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quackst : ScriptableObject
+{
+    //[System.Serializable]
+
+    //public struct Info
+    //{
+    //    public string Name;
+    //    public string Description;
+    //}
+
+    //[Header("Info")] public Info Information;
+}
