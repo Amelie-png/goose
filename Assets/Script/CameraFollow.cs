600f1148e0b69b129c664fff4c9cc8b410f8019a
Alisa Dubina <adubina24@stu.aberdeenhall.com>
2024-05-14 09:21:22 -0700

    borders for the moving camera added :0

--- Assets/Script/CameraFollow.cs
+++ Assets/Script/CameraFollow.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform target; 
+    public Transform target;
     // Update is called once per frame
     void Update() {
-        transform.position = new Vector3(target.transform.position.x, target.transform.position.y, -1);
+        float x = transform.position.x, y = transform.position.y;
+
+        float t = target.transform.position.y;
+        if (t < 2.05 && t  > - 2.04) 
+            y = t;
+
+        t = target.position.x;
+        if (t < 3.05 && t > -3.05)
+            x = t;
+                                                    
+        transform.position = new Vector3(x, y, -1);
 
     } // Update
 }
