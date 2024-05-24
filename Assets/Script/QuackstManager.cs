78ed7a9badc4d5aded899dc9468607a82d5ab521
Alisa Dubina <adubina24@stu.aberdeenhall.com>
2024-05-24 09:20:33 -0700

    Revert "Quackst opens and closes now, forever (almost)"
    This reverts commit 373a2cc2abe3f5c50edbd2078c83f0ef68193a5d.

--- Assets/Script/QuackstManager.cs
+++ Assets/Script/QuackstManager.cs
@@ -4,29 +4,15 @@ using UnityEngine;
 
 public class QuackstManager : MonoBehaviour
 {
-    public Animator animator;
-
     // Start is called before the first frame update
-    void Start() {
+    void Start()
+    {
         
-    } // Start
-
-    public void StartQuackst() {
-        animator.SetBool("IsOpen", true);
-
-        Debug.Log("Quackst opened.");
-    } // StartDialogue
+    }
 
     // Update is called once per frame
-    void Update() {
-        if (Input.GetKeyDown(KeyCode.Q)) {
-            EndQuackst();
-        } // if
-
-    } // Update
-
-    void EndQuackst() {
-        animator.SetBool("IsOpen", false);
-        Debug.Log("Quackst closed.");
-    } // EndQuackst
+    void Update()
+    {
+        
+    }
 }
