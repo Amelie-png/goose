08ef22e4ffff3334b686720a00ed6fd347745605
Alisa Dubina <adubina24@stu.aberdeenhall.com>
2024-05-30 13:51:16 -0700

    Dialogue Image works nowcd git-learning/goosecd git-learning/goose(almost)

--- Assets/Script/Managers/QuackstManager.cs
+++ Assets/Script/Managers/QuackstManager.cs
@@ -5,18 +5,13 @@ using UnityEngine;
 public class QuackstManager : MonoBehaviour
 {
     public Animator animator;
-    public CanvasGroup quackst;
 
     // Start is called before the first frame update
     void Start() {
-        quackst.alpha = 0f;
-        quackst.blocksRaycasts = false;
     } // Start
 
     public void StartQuackst() {
         animator.SetBool("IsOpen", true);
-        quackst.alpha = 1f;
-        quackst.blocksRaycasts = true;
 
         Debug.Log("Quackst opened.");
     } // StartDialogue
@@ -28,8 +23,6 @@ public class QuackstManager : MonoBehaviour
 
     public void EndQuackst() {
         animator.SetBool("IsOpen", false);
-        quackst.alpha = 0f;
-        quackst.blocksRaycasts = false;
         Debug.Log("Quackst closed.");
     } // EndQuackst
 }
