aec6e3c3298659a5b4d19391dab6c21126767f60
Amelie Zhang <amelie.ry.zh@gmail.com>
2024-06-11 18:10:23 -0700

    Bob talks

--- Assets/Script/Dialogue/Line.cs
+++ Assets/Script/Dialogue/Line.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public class Dialogue {
-
-    public Line[] sentences;
-} // Dialogue
+public class Line{
+    public string character;
+    public Sprite sprite;
+    public string speech;
+}
